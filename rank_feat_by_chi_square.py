1454a5d47febaac2a9fb74cad98371838fde5bab
nemo <nemo@dreammaster.local>
2011-02-01 15:37:03 -0800

    still whole numbers, etc.

--- rank_feat_by_chi_square.py
+++ rank_feat_by_chi_square.py
@@ -7,18 +7,14 @@ doc_freq = {} #feature: number of documents it appears in
 table_3 = {} #feature : (top,bottom)
 chi_squared = {}
 instance_count = 0
-total_in_class = {}
+total_in_class = {} # total number of features that appear in a certain class
 
 for instance in sys.stdin.readlines():
 	instance_count +=1
 	instance = instance.split()
-	
-	#is this now pointless?
-	seen_in_doc = set() #features in this doc
-	
-	path = instance[0]
 	classification = instance[1]
 	features = instance[2::2]	
+	
 	if classification in classes:
 		class_counts[classification] += 1
 	else:
@@ -29,26 +25,22 @@ for instance in sys.stdin.readlines():
 	for f in features:
 		total_in_class[classification] += 1
 		if f not in global_features:
+			doc_freq[f] = 1
 			global_features.add(f)
 			features_in_classes[f] = {}
 			features_in_classes[f]['__classes__'] = set()
+		else:
+			doc_freq[f] += 1
 		if classification in features_in_classes[f]['__classes__']:
 			features_in_classes[f][classification] += 1
 		else:
 			features_in_classes[f]['__classes__'].add(classification)
 			features_in_classes[f][classification] = 1
-		
-		if f not in seen_in_doc:  #this entire set seems pointless now, is that correct?
-			seen_in_doc.add(f)
-			try:					#ugly way of doing this... worse than passing around lots of sets?
-				doc_freq[f]+=1
-			except KeyError:
-				doc_freq[f] = 1
 
 
 #build table 3 per feature
 for feature in global_features:
-	#fill in missing features
+	# #fill in missing features
 	for missing_class in classes-features_in_classes[feature]['__classes__']:
 		features_in_classes[feature][missing_class] = 0
 	table_3[feature] = [] #list of tuples (top_row,bottom_row), all classes
@@ -68,15 +60,16 @@ for feature in global_features:
 
 
 for feature in global_features:
-	to_sum = []	
+	to_sum = []
+
 	#looping through table 3 will loop through all classifications
 	index = -1 #just to initialize
 	for classification in classes:
 		index+=1
 		class_count,not_feature = table_3[feature][index]
 		try:
-			expected = total_in_class[classification]/class_count / 2
-			observed = class_count-not_feature
+			expected = class_count / 2
+			observed = features_in_classes[feature][classification]
 			to_sum.append(((observed-expected)**2)/expected)
 		except ZeroDivisionError:  #i'm sure there are better ways to do this
 			sys.stderr.write("ZeroDivisionError, no need for concern")

5e0bfb73dcbe8bfbb413dee2bf8a986176b417c0
nemo <nemo@dreammaster.local>
2011-02-01 16:55:25 -0800

    this seems more reasonable

--- rank_feat_by_chi_square.py
+++ rank_feat_by_chi_square.py
@@ -8,6 +8,7 @@ table_3 = {} #feature : (top,bottom)
 chi_squared = {}
 instance_count = 0
 total_in_class = {} # total number of features that appear in a certain class
+N = 0
 
 for instance in sys.stdin.readlines():
 	instance_count +=1
@@ -16,26 +17,27 @@ for instance in sys.stdin.readlines():
 	features = instance[2::2]	
 	
 	if classification in classes:
-		class_counts[classification] += 1
+		class_counts[classification] += 1.0
 	else:
 		classes.add(classification)
 		total_in_class[classification] = 0
-		class_counts[classification] = 1
+		class_counts[classification] = 1.0
 	
 	for f in features:
-		total_in_class[classification] += 1
+		N += 1.0
+		total_in_class[classification] += 1.0
 		if f not in global_features:
-			doc_freq[f] = 1
+			doc_freq[f] = 1.0
 			global_features.add(f)
 			features_in_classes[f] = {}
 			features_in_classes[f]['__classes__'] = set()
 		else:
-			doc_freq[f] += 1
+			doc_freq[f] += 1.0
 		if classification in features_in_classes[f]['__classes__']:
-			features_in_classes[f][classification] += 1
+			features_in_classes[f][classification] += 1.0
 		else:
 			features_in_classes[f]['__classes__'].add(classification)
-			features_in_classes[f][classification] = 1
+			features_in_classes[f][classification] = 1.0
 
 
 #build table 3 per feature
@@ -57,23 +59,32 @@ for feature in global_features:
 #     withF <- cnt(class, feature)
 #     withoutF <- cnt(class) - cnt(class, feature)
 #     score <- score + [withF-expected]^2/expected
+#     score <- score + [withoutF-expected]^2/expected
 
 
 for feature in global_features:
 	to_sum = []
-
-	#looping through table 3 will loop through all classifications
 	index = -1 #just to initialize
+	
+	#row total is the total number of documents of any class that had the feature
+	row_total = doc_freq[feature]
+	
 	for classification in classes:
 		index+=1
 		class_count,not_feature = table_3[feature][index]
 		try:
-			expected = class_count / 2
+			column_total = total_in_class[classification]
+			expected = row_total * column_total / N
 			observed = features_in_classes[feature][classification]
 			to_sum.append(((observed-expected)**2)/expected)
+			# withF = features_in_classes[feature][classification]
+			# withoutF = table_3[feature][index][1]
+			# to_sum.append(((withF-expected)**2)/expected)
+			# to_sum.append(((withoutF-expected)**2)/expected)
+			
 		except ZeroDivisionError:  #i'm sure there are better ways to do this
 			sys.stderr.write("ZeroDivisionError, no need for concern")
 	chi_squared[feature] = sum(to_sum)
 
 for key in sorted(chi_squared.keys(), key=chi_squared.get, reverse=True):
-	print key +" "+ str(chi_squared[key]) + " " + str(doc_freq[key])
+	print key +" "+ str(chi_squared[key]) + " " + str(int(doc_freq[key]))
